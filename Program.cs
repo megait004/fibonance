8df793e1c81e099569b89218b2cdfdb2a233d84f
Nguyễn Nguyên Giáp <103838138+megait004@users.noreply.github.com>
2024-05-10 00:20:42 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai5_lap2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n;
+            do
+            {
+                Console.WriteLine("vui long nhap n >2");
+               n= Convert.ToInt32(Console.ReadLine());
+                if (n > 2)
+                {
+                    int f1;
+                    int f2;
+                    f1 = 1;
+                    f2 = 1;
+                    Console.Write("{0} {1}",f1, f2);
+                    for(int i = 3; i <= n; i++)
+                    {
+                        int f;
+                        f = f1 + f2;
+                        Console.Write(" {0}", f);
+                        f1 = f2;
+                        f2 = f;
+                    }
+                }
+            }
+            while (n<=2);
+            Console.ReadKey();
+        }
+    }
+}
